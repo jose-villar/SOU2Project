80bb7ad8085d87d4c000195a26daa93b5004e760
Felipe <felipe.ortiz.san@gmail.com>
2020-07-10 04:30:52 +0000

    Hola

--- main.c
+++ main.c
@@ -12,7 +12,7 @@
 #define nOfLabsR1 3 // number of labs in region 1
 #define nOfLabsR2 2 // number of labs in region 2
 
-pthread_mutex_t mutex;
+pthread_mutex_t mutex; //MUTEX
 
 // Function executed my each doctor thread. It registers the number of dead patients a doctor had in one day.
 void *notifyDeaths(void *i){
@@ -23,7 +23,7 @@ void *notifyDeaths(void *i){
   nOfDeaths = rand() % 100; // number between 0 and 99 
   filePointer = fopen("deaths.txt", "a+");  // open the file for writing in appending mode
   
-  pthread:pthread_mutex_lock(&mutex);
+  pthread_mutex_lock(&mutex); //MUTEX LOCK
   printf("The doctor number: %ld is registering %d deaths... \n", doctorId, nOfDeaths);
   sleep(4);
 
@@ -31,7 +31,7 @@ void *notifyDeaths(void *i){
   //fprintf(filePointer, "%c", '\n');
   fclose(filePointer); // closes the file
 
-  pthread_mutex_unlock(&mutex);
+  pthread_mutex_unlock(&mutex); //MUTEX UNLOCK
 
   pthread_exit(NULL);
 }
@@ -45,7 +45,7 @@ void *notifyCases(void *i){
   nOfCases = rand() % 100; // number between 0 and 99 
   filePointer = fopen("cases.txt", "a+");  // open the file for writing in appending mode
   
-  pthread:pthread_mutex_lock(&mutex);
+  pthread_mutex_lock(&mutex);
   printf("The lab number: %ld is registering %d new cases... \n", labId, nOfCases);
   sleep(4); 
   
@@ -56,14 +56,30 @@ void *notifyCases(void *i){
   pthread_mutex_unlock(&mutex);
 
   pthread_exit(NULL);
-  
+
 }
 
+void *healthServiceFunction(){
+  int toSleep = rand() % 10;
+  printf("healthServiceFunction");
+  sleep(toSleep);
+}
 
+void *healthDepartmentFunction(){
+  int toSleep = rand() % 10;
+  printf("healthDepartmentFunction");
+  sleep(toSleep);
+}
 
 int main(int argc, char *argv[]) {
   pthread_t doctors [nOfDoctorsR1 + nOfDoctorsR2];
   pthread_t labs [nOfLabsR1 + nOfLabsR2];
+
+  pthread_t hServiceR1; 
+  pthread_t hServiceR2;
+
+  pthread_t healthDepartment;
+  
   srand (time(NULL));
   pthread_mutex_init(&mutex, NULL);
 
@@ -81,8 +97,8 @@ int main(int argc, char *argv[]) {
     pthread_join(doctors[i], NULL);   
   }
 
-  printf("---------------------------------------------- \n"); 
-  
+  printf("--------------------------------------------\n");
+
   //Creating laboratories threads
   for(long i=0; i<nOfLabsR1 + nOfLabsR2; i++){
     //printf("I'm the lab number: %ld \n", i);
@@ -90,10 +106,102 @@ int main(int argc, char *argv[]) {
       printf("Error during thread creation \n");
     }
   }
+
   // Wait for all the labs threads to finish
   for(int i = 0; i < nOfLabsR1 + nOfLabsR2; i++) {
     pthread_join(labs[i], NULL);   
   }
+
+  //Creating Health Service threads
+  pthread_create(&hServiceR1, NULL, healthServiceFunction, NULL);
+  pthread_join(hServiceR1, NULL);
+
+  pthread_create(&hServiceR2, NULL, healthServiceFunction, NULL);
+  pthread_join(hServiceR2, NULL);
+
+  pthread_create(&healthDepartment, NULL, healthDepartmentFunction, NULL);
+
+  return 0;
+}
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+void clearArr(int arr[], int size) {
+  for(int i = 0; i < size; i++) {
+    arr[i] = -1;
+  }
+}
+
+int juntarNumeros(int arr[], int size){
+  int numero=0;
+  int contador = 0;
+  int potencia;
+
+  for(int i=0; i < size; i++){
+    if(arr[i]!=-1){
+      contador+=1;
+    }
+  }
+  for(int i=0; i < size; i++){
+    if(arr[i]!=-1){
+      potencia = pow(10, contador-1);
+      numero=numero+arr[i]*potencia;
+      contador-=1;
+    }
+  }
+
+  return numero;
+}
+
+
+int main(void) {
+  FILE *fp;
+  char c; 
+  fp =  fopen("deaths.txt", "r");  
+  //hola
+  int n;
+  int contador;
+  int arr[5] = {-1,-1,-1,-1,-1};
+  int suma=0;
+  //int arr1[5] = {3,7,4,8,-1};
+  //int numero = juntarNumeros(arr1, 5);
+  //printf("el valor es %d \n", numero);
+
+  int i = 0;
+
+  while(1) {
+    c = fgetc(fp);
+    if( feof(fp) ) { 
+      break ;
+    }
+    //printf("%c", c);
+    if(c != '\n'){
+      n = c - '0';
+      arr[i]=n;
+      i++;
+      printf("numero: %d \n", n);
+    }
+    else{
+      printf("El valor leido es %d\n", juntarNumeros(arr, 5));
+      suma=suma+juntarNumeros(arr, 5);
+      printf("la suma es %d\n", suma);
+      clearArr(arr, 5);
+
+    }    
+  }
+  
+  fclose(fp);
+
+  fp =  fopen("deaths.txt", "w");
+  fprintf(fp, "%d\n", suma); 
+ 
+
   
   return 0;
 }
+*/
